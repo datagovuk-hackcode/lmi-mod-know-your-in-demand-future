4d003e47df1c68addb09eba9efa53b87d9c6824d
Jimmy Tide <jimmytidey@gmail.com>
2014-06-23 12:58:35 +0100

    adding sub jobs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,140 +20,140 @@
 lmi = {};
 lmi.jobs = [];
 lmi.ready=[];
+lmi.sub_jobs = [];
 
 $(document).ready(function(){ 
-    $('#region_selector').change(function(){
-        var region_code = $(this).val();
+  $('#region_selector').change(function(){
+    var region_code = $(this).val();
 
-        $.getJSON('http://api.lmiforall.org.uk/api/v1/ess/regions/ranksocs/'+region_code + "?callback=?", function(data){ 
-            lmi.jobs = [];
-            lmi.jobs = data.slice(0, 10);
-            lmi.regionResults();   
-        }); 
+    //get a list of the most indemand jobs by soc code
+    $.getJSON('http://api.lmiforall.org.uk/api/v1/ess/regions/ranksocs/'+region_code + "?callback=?", function(data){ 
+        lmi.jobs = [];
+        lmi.jobs = data.slice(0, 4);
+        lmi.regionResults();      
     });
+})
 });
 
 lmi.regionResults = function() { 
-    
-    $('#results_container').html('');
 
-    
-    $.each(lmi.jobs, function(key, val){ 
-          
-        $.getJSON('http://api.lmiforall.org.uk/api/v1/soc/code/' + val.soc + "?callback=?", function(data) { 
-            lmi.jobs[key].soc_data = data;
+  $('#results_container').html('');
 
-        });
+  //loop through the list of soc codes we just got
+    $.each(lmi.jobs, function(job_key, job_val){ 
+
+        //get title of each of the most in demand jobs for which we only have soc codes
+        $.getJSON('http://api.lmiforall.org.uk/api/v1/soc/code/' + job_val.soc + "?callback=?", function(data) { 
+
+            job_val.job_cat = cat_title = data.title; 
+            job_val.sub_jobs = [];
+
+            //do a search for jobs with similar titles 
+            $.getJSON('http://api.lmiforall.org.uk/api/v1/soc/search?q='+data.title + "&callback=?", function(search_data){ 
                 
-    });
+                //for each job with a similar title, get all the data we want
+                $.each(search_data, function(sub_job_key,sub_job_val){
+                    if(sub_job_key<3){
+                        job_val.sub_jobs.push(sub_job_val);
+                        
+                        $.getJSON('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=' + sub_job_val.soc + "&callback=?", function(data) {
+                            lmi.jobs[job_key].sub_jobs[sub_job_key].ashe_data = data;
+                        });
 
-
-
-
-    $.each(lmi.jobs, function(key, val){
-        $.getJSON('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=' + val.soc + "&callback=?", function(data) {
-            lmi.jobs[key].ashe_data = data;
-            console.log("----> " + key);
-            console.log("----> " + key);
-            if(key == lmi.jobs.length-1) { 
-                lmi.renderRegionResults();
-                
-            }
-        });         
-    });  
-    
-    
-    $.each(lmi.jobs, function(key, val){ 
-        $.getJSON('http://api.lmiforall.org.uk/api/v1/wf/predict?minYear=2012&maxYear=2020&soc=' + val.soc + "&callback=?", function(data) {
-            lmi.jobs[key].wf_data = data;
+                        $.getJSON('http://api.lmiforall.org.uk/api/v1/wf/predict?minYear=2012&maxYear=2020&soc=' + sub_job_val.soc + "&callback=?", function(data) {
+                            lmi.jobs[job_key].sub_jobs[sub_job_key].wf_data = data;
+                            if(sub_job_key == lmi.jobs[job_key].sub_jobs.length-1 &&
+                                job_key == lmi.jobs.length-1) { 
+                                lmi.renderRegionResults();
+                            }
+                        });
+                    }
+                });
+            });    
         });
-    });    
+    })
+
+
+
 }
 
 
-lmi.jobDetails = function(soc) { 
-    
-    console.log(lmi.jobs);
-    $.each(lmi.jobs, function(key,val) {
-        
-        lmi.current_item = val;
-        
-        if(val.soc_data.soc == soc) {
-            var content      =  '<h2 data-soc="'+soc+'">' + val.soc_data.title + "</h2>";
-            content          +=  '<p>' + val.soc_data.description + "</p>";
-            content          +=  '<p><strong>Vacancies that are hard to fill:</strong> ' + parseInt(val.percentHTF) + "%</p>";
-            content          +=  '<p><strong>Vacancies unfilled due to skills shortage: </strong> ' + parseInt(val.percentSSV) + "%</p>";
-            content          +=  '<h3>Qualifications</h3>';
-            content          +=  '<p>' + val.soc_data.qualifications + "</p>";
-            content          +=  '<h3>Pay</h3>';
-            content          +=  '<ul class="pay">';
-            
-            console.log(val.ashe_data); 
+lmi.jobDetails = function(soc) {
 
-            $.each(val.ashe_data.series, function(ashe_key, ashe_val) {
-                content          +=  '<li>' + ashe_val.year + ' : £' + parseInt(ashe_val.estpay) + ' per week</li>';
-            });
-            
-            content          +=  '</ul>';
-            content          +=  '<h3>Forecast</h3>';
-            content          +=  '<select id="graph_drop_down">';
-            content          +=  '<option value="">Total</option>';
-                content          +=  '<option value="gender">Gender</option>';
-                content          +=  '<option value="status">Status</option>';
-            content          +=  '<select>';
-            content          +=  '<div id="forecast"></div>';
-            
 
-            
-            
-            $('#job_detail').html(content);
-            
-            var graph_filter = $('#graph_drop_down').val();
-            
-            lmi.drawGraph(val, graph_filter);
+    $.each(lmi.sub_jobs, function(key,val) {
 
-            $('#graph_drop_down').change(function(){ 
-                
-                var graph_filter = $('#graph_drop_down').val();
-                lmi.drawGraph(lmi.current_item, graph_filter);
-            });
+      lmi.current_item = val;
 
-            $('#instructions').css('display', "none");
-        }
-    });
+      if(val.soc == soc) {
+        var content      =  '<h2 data-soc="'+soc+'">' + val.title + "</h2>";
+        content          +=  '<p>' + val.description + "</p>";
+        content          +=  '<p><strong>Vacancies that are hard to fill:</strong> ' + parseInt(val.percentHTF) + "%</p>";
+        content          +=  '<p><strong>Vacancies unfilled due to skills shortage: </strong> ' + parseInt(val.percentSSV) + "%</p>";
+        content          +=  '<h3>Qualifications</h3>';
+        content          +=  '<p>' + val.qualifications + "</p>";
+        content          +=  '<h3>Pay</h3>';
+        content          +=  '<ul class="pay">';
+
+        $.each(val.ashe_data.series, function(ashe_key, ashe_val) {
+            content          +=  '<li>' + ashe_val.year + ' : £' + parseInt(ashe_val.estpay) + ' per week</li>';
+        });
+
+        content          +=  '</ul>';
+        content          +=  '<h3>Forecast</h3>';
+        content          +=  '<select id="graph_drop_down">';
+        content          +=  '<option value="">Total</option>';
+        content          +=  '<option value="gender">Gender</option>';
+        content          +=  '<option value="status">Status</option>';
+        content          +=  '<select>';
+        content          +=  '<div id="forecast"></div>';
+
+        $('#job_detail').html(content);
+
+        var graph_filter = $('#graph_drop_down').val();
+
+        lmi.drawGraph(val, graph_filter);
+
+        $('#graph_drop_down').change(function(){ 
+
+          var graph_filter = $('#graph_drop_down').val();
+          lmi.drawGraph(lmi.current_item, graph_filter);
+      });
+
+        $('#instructions').css('display', "none");
+    }
+});
 }
 
 
 
 
 lmi.drawGraph = function(val, graph_filter) { 
-    lmi.graph_data = [];
+  lmi.graph_data = [];
     //graph_filter= '';
     
     
     $('#forecast').replaceWith("<div id='forecast'></div>");
-    console.log(graph_filter);
-    if(graph_filter == '') { 
- 
-        var offset = val.wf_data.predictedEmployment[0].employment * 0.8; 
     
-        $.each(val.wf_data.predictedEmployment, function(key, val) { 
-           var employment = parseInt(val.employment-offset);
-           lmi.graph_data[key] = {'year': val.year.toString(), "employment" : employment }; 
-        });
-        lmi.morris();
-    }
-    
-    
-    
-    else if(graph_filter == 'gender') { 
-        $.getJSON('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/' + graph_filter + '?soc=' + val.soc, function(data) {
-            
-        console.log(data);
+    if(graph_filter == '') {
+
+      var offset = val.wf_data.predictedEmployment[0].employment * 0.8; 
+
+      $.each(val.wf_data.predictedEmployment, function(key, val) { 
+       var employment = parseInt(val.employment-offset);
+       lmi.graph_data[key] = {'year': val.year.toString(), "employment" : employment }; 
+   });
+      lmi.morris();
+  }
+
+
+  else if(graph_filter == 'gender') { 
+      $.getJSON('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/' + graph_filter + '?soc=' + val.soc, function(data) {
+
+
         var offset = data.predictedEmployment[0].breakdown[0].employment * 0.8; 
 
         $.each(data.predictedEmployment, function(key, val) { 
-            console.log(val);
             var employment = parseInt(val.employment) - offset;
             var male = parseInt(val.breakdown[0].employment);
             var female = parseInt(val.breakdown[1].employment);
@@ -161,30 +161,30 @@ lmi.drawGraph = function(val, graph_filter) {
         });
 
         lmi.morrisGender();
-         });
-    }  
-    
-    else if(graph_filter == 'status') { 
-        $.getJSON('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/' + graph_filter + '?soc=' + val.soc, function(data) {
-            
+    });
+  }  
+
+  else if(graph_filter == 'status') { 
+      $.getJSON('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/' + graph_filter + '?soc=' + val.soc, function(data) {
+
         console.log(data);
         var offset = data.predictedEmployment[0].breakdown[0].employment * 0.8; 
 
         $.each(data.predictedEmployment, function(key, val) { 
-            console.log(val);
-            var employment = parseInt(val.employment) - offset;
-            var ft = parseInt(val.breakdown[0].employment);
-            var pt = parseInt(val.breakdown[1].employment);
-            var se = parseInt(val.breakdown[2].employment);
-            
-            lmi.graph_data[key] = {'year': val.year.toString(), "FT Employee" : ft, "PT Employee" : pt, "Self Employed" : se }; 
-        });
+          console.log(val);
+          var employment = parseInt(val.employment) - offset;
+          var ft = parseInt(val.breakdown[0].employment);
+          var pt = parseInt(val.breakdown[1].employment);
+          var se = parseInt(val.breakdown[2].employment);
+
+          lmi.graph_data[key] = {'year': val.year.toString(), "FT Employee" : ft, "PT Employee" : pt, "Self Employed" : se }; 
+      });
 
         lmi.morrisStatus();
-         });
-    }  
-    
-  
+    });
+  }  
+
+
 }
 
 lmi.morris = function() { 
@@ -201,7 +201,7 @@ lmi.morris = function() {
       // Labels for the ykeys -- will be displayed when you hover over the
       // chart.
       labels: ['employment']
-    });    
+  });    
 }
 
 lmi.morrisGender = function() { 
@@ -218,7 +218,7 @@ lmi.morrisGender = function() {
       // Labels for the ykeys -- will be displayed when you hover over the
       // chart.
       labels: ['male', 'female']
-    });    
+  });    
 }
 
 lmi.morrisStatus = function() { 
@@ -235,23 +235,26 @@ lmi.morrisStatus = function() {
       // Labels for the ykeys -- will be displayed when you hover over the
       // chart.
       labels: ['FT Employee', 'PT Employee', 'Self Employed']
-    });    
+  });    
 }
 
 lmi.attachJobClicks = function() { 
     $('#results_container li').click(function(){ 
-        var soc = $(this).attr('data-soc');  
-        lmi.jobDetails(soc);
-    });   
+      var soc = $(this).attr('data-soc');  
+      lmi.jobDetails(soc);
+  });   
 }
 
 lmi.renderRegionResults = function() { 
-    $.each(lmi.jobs, function(key, val){ 
-          
-        var content      =  '<li data-soc="' + lmi.jobs[key].soc_data.soc +  '">' + lmi.jobs[key].soc_data.title + "</li>";
+    
+    $.each(lmi.jobs, function(job_key, val){
+        $('#results_container').append('<li></li><ol>');
+        var content  =  '<li data-soc="' + lmi.sub_jobs[key].soc +  '">' + lmi.sub_jobs[key].title + "</li>";
         $('#results_container').append(content);
+        
+        $('#results_container').append('</ol>');
+
         lmi.attachJobClicks();
-                
     }); 
 }
 
